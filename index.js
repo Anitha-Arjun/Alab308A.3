2f703b1bf9bb00da823740788b971a80b06ad916
Anitha-Arjun <arjunmurugan@Arjuns-MacBook-Air.local>
2024-08-28 14:27:15 -0500

    first Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// Importing database functions. DO NOT MODIFY THIS LINE.
+import { central, db1, db2, db3, vault } from "./databases.js";
+
+//creating async function
+async function getUserData(id) {
+  const dbs = {
+    db1: db1,
+    db2: db2,
+    db3: db3,
+  };
+
+  //find users in the database using central
+  const dataBaseName = await central(id);
+  console.log(dataBaseName);
+  //User's basic information
+  const userInfo = await dbs[dataBaseName](id);
+  console.log(userInfo);
+  //access the vault to fetch the personal data about the user
+  const personalData = await vault(id);
+  console.log(personalData);
+  return {
+    ...userInfo,
+    ...personalData,
+  };
+}
+
+const user = await getUserData(9);
+console.log(user);
